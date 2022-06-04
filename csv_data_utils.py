defeb90f25f082f6d35f95e7c6e97fc8e01f1243
Andrew <rakovandrew812@gmail.com>
2022-06-05 02:52:12 +0300

    saved mean csv data for all videos to train nn

--- csv_data_utils.py
+++ csv_data_utils.py
@@ -2,11 +2,12 @@ import configparser
 import os
 import shutil
 
+import pandas as pd
 from feat import Detector
-
+from feat.utils import read_feat
 
 face_model = "retinaface"
-landmark_model = "psafld"
+landmark_model = "pfld"
 emotion_model = "fer"
 detector = Detector(
     face_model=face_model,
@@ -15,10 +16,11 @@ detector = Detector(
 )
 
 config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def extract_and_save_csv_data_from_video(video, video_directory, csv_directory):
-    new_file_name = os.path.splitext(video)[0] + '.csv'
+    new_file_name = video.replace('.mp4', '.csv')
     video_prediction = detector.detect_video(os.path.join(video_directory, video),
                                              outputFname=new_file_name)
     shutil.move(new_file_name,
@@ -33,13 +35,42 @@ def extract_csv_data_from_all_videos(video_directory, csv_directory):
                                              csv_directory)
 
 
-def extract_csv_data_from_all_fake_videos(fake_pleasure_video_directory=config['Paths']['fake pleasure video directory'],
-                                          fake_pleasure_csv_directory=config['Paths']['fake pleasure csv directory']):
+def extract_csv_data_from_all_fake_videos(
+        fake_pleasure_video_directory=config['Paths']['fake pleasure video directory'],
+        fake_pleasure_csv_directory=config['Paths']['fake pleasure csv directory']):
     extract_csv_data_from_all_videos(fake_pleasure_video_directory,
                                      fake_pleasure_csv_directory)
 
 
-def extract_csv_data_from_all_true_videos(true_pleasure_video_directory=config['Paths']['true pleasure video directory'],
-                                          true_pleasure_csv_directory=config['Paths']['true pleasure csv directory']):
+def extract_csv_data_from_all_true_videos(
+        true_pleasure_video_directory=config['Paths']['true pleasure video directory'],
+        true_pleasure_csv_directory=config['Paths']['true pleasure csv directory']):
     extract_csv_data_from_all_videos(true_pleasure_video_directory,
                                      true_pleasure_csv_directory)
+
+
+def remove_specific_row(file, new_file, columns_with_values):
+    csv_file = pd.read_csv(file)
+    for key, value in columns_with_values.items():
+        csv_file = csv_file[eval("csv_file.{}".format(key)) >= value]
+    csv_file.to_csv(new_file, index=False)
+
+
+def save_specific_column(file, new_file, columns):
+    csv_file = pd.read_csv(file)
+    csv_file = csv_file[columns]
+    csv_file.to_csv(new_file, index=False)
+
+
+def find_mean_by_columns(file, new_file):
+    feat = read_feat(file)
+    feat = feat.extract_mean()
+    feat.to_csv(new_file, index=False)
+
+
+def find_and_save_emotion_duration(file, new_file):
+    csv_file = pd.read_csv(file)
+    new_column = csv_file['AU43'] + 1
+    csv_file['duration'] = new_column
+    csv_file['duration'] = csv_file.shape[0] / 30
+    csv_file.to_csv(new_file, index=False)
--- video_utils.py
+++ video_utils.py
@@ -1,6 +1,14 @@
+import configparser
+import os.path
 import time
+from os.path import join
 
 import cv2
+import pandas as pd
+from feat.utils import read_feat
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def play_video(video):
@@ -24,3 +32,43 @@ def play_video(video):
             break
     cap.release()
     cv2.destroyAllWindows()
+
+
+def save_video_frames_and_aus_activity(video):
+    cap = cv2.VideoCapture(video)
+    csv = video.replace('video', 'csv').replace('.mp4', '.csv')
+    file = pd.read_csv(csv)
+    aus = file["AU06"], file["AU07"], file["AU12"], file["AU25"], file["AU26"]
+    aus_info = open(os.path.join(config['Paths']['temp directory'], 'aus_info.txt'), 'w')
+    i = 0
+
+    if not cap.isOpened():
+        print("Error File Not Found")
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        j = 0
+
+        if ret == True:
+
+            cv2.putText(img=frame, text=str(i), org=(40, 50), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=2,
+                        color=(0, 255, 0), thickness=3)
+            cv2.imwrite(os.path.join(config['Paths']['temp directory'], 'frame' + str(i) + '.jpg'), frame)
+            cv2.imshow('frame', frame)
+
+            aus_info.write(str(i)+'\n')
+            while j < 5:
+                aus_info.write(str(aus[j][i])+'\n')
+                j += 1
+            print(i)
+
+            i += 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    aus_info.close()
